e3102a9bfb14c2d0d577af277f2bab02aaec4e16
prajwalshenoy <prajwalkpshenoy@gmail.com>
2020-10-01 22:25:54 +0530

    setup initialise.sh to install scraper

--- initialise.sh
+++ initialise.sh
@@ -3,4 +3,6 @@ pip3 install virtualenv
 virtualenv `pwd`/web-scraper
 cd web-scraper
 git clone --single-branch --branch develop https://github.com/PrajwalShenoy/StaticWebScraper.git
+mv `pwd`/StaticWebScraper/files/geckodriver `pwd`/bin
+cd ..
 `pwd`/web-scraper/bin/pip3 install git+https://github.com/PrajwalShenoy/StaticWebScraper.git@develop
