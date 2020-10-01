445ca38b6c52e1d78dcff3e0d153af7d59755954
prajwalshenoy <prajwalkpshenoy@gmail.com>
2020-10-01 22:04:59 +0530

    testing

--- initialise.sh
+++ initialise.sh
@@ -1,4 +1,6 @@
 pip3 install virtualenv
 
 virtualenv `pwd`/web-scraper
+cd web-scraper
+git clone --single-branch --branch develop https://github.com/PrajwalShenoy/StaticWebScraper.git
 `pwd`/web-scraper/bin/pip3 install git+https://github.com/PrajwalShenoy/StaticWebScraper.git@develop
